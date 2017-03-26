b3b4561920d6aab740be60ca13f756219c300624
finnhodgkin <finnhodgkin@gmail.com>
2017-03-26 12:59:15 +0100

    fix handler bug that was allowing 2 headers to be sent for one response also fixed the test script to limit test messages to a set amount of characters fixes #6 and #5

--- src/handler.js
+++ src/handler.js
@@ -5,14 +5,24 @@ const handler = module.exports = {};
 
 handler.serveStatic = (req, res, page) => {
 
-  res.writeHead(200, {'content-type':'text/html'});
-
   const filePath = path.join(__dirname, '..', 'public', page);
   const pageContents = fs.createReadStream(filePath);
+  
+  pageContents.on('open', () => {
+    pageContents.pipe(res);
+  });
+
+  let headerNotSet = true;
+  pageContents.on('data', () => {
+    if (headerNotSet) {
+      res.writeHead(200, {'content-type':'text/html'});
+      headerNotSet = false;
+    }
+  });
 
   pageContents.on('error', (err) => {
     handler.serveError(req, res, err);
-  }).pipe(res);
+  });
 
 };
 
@@ -25,7 +35,7 @@ handler.cool = (req, res) => {
 };
 
 handler.servePublic = (req, res) => {
-  
+
 };
 
 handler.serveError = (req, res, error) => {
